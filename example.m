9f6fe0e0a90f90b9779847e47400a3038ab56db3
Xin Liu <34627481+starxliu@users.noreply.github.com>
2017-12-17 11:51:34 -0500

    Update example.m

--- example.m
+++ example.m
@@ -5,6 +5,26 @@
 % Copyright @ He Zhang and Xin Liu, 2017.
 % This software is free for academic usage under the permissive BSD license. If you publish results obtained using this software, please cite our paper.
 
+% @article{liu2017efficient,
+%   title={Efficient single image dehazing and denoising: An efficient multi-scale correlated wavelet approach},
+%   author={Liu, Xin and Zhang, He and Cheung, Yiu-ming and You, Xinge and Tang, Yuan Yan},
+%   journal={Computer Vision and Image Understanding},
+%   volume={162},
+%   pages={23--33},
+%   year={2017}
+% }
+% 
+% @article{liu2016scene,
+%   title={Scene-adaptive single image dehazing via opening dark channel model},
+%   author={Liu, Xin and Zhang, He and Tang, Yuan Yan and Du, Ji-Xiang},
+%   journal={IET Image Processing},
+%   volume={10},
+%   number={11},
+%   pages={877--884},
+%   year={2016}
+% }
+
+
 clear all; close all; clc;
 
 f = im2double(imread('image/hqu_village.png'));
